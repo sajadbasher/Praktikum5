7abd067e997c948de4426bcd42f48e1f3fae9bdc
Amar-8 <84033938+Amar-8@users.noreply.github.com>
2023-11-09 16:23:50 +0100

    Update app.js
    updating the js code

--- js/app.js
+++ js/app.js
@@ -9,39 +9,25 @@ function getUserMedia(options, successCallback, failureCallback) {
   throw new Error('User Media API not supported.');
 }
 
-function trackLocation() {
-  if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(showPosition, showError, {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
+var target = document.getElementById('target');
+var watchId;
+
+function appendLocation(location, verb) {
+  verb = verb || 'updated';
+  var newLocation = document.createElement('p');
+  newLocation.innerHTML = 'Location ' + verb + ': ' + location.coords.latitude + ', ' + location.coords.longitude + '';
+  target.appendChild(newLocation);
+}
+
+if ('geolocation' in navigator) {
+  document.getElementById('askButton').addEventListener('click', function () {
+    navigator.geolocation.getCurrentPosition(function (location) {
+      appendLocation(location, 'fetched');
     });
-  } else {
-    document.getElementById('locationDisplay').textContent = 'Geolocation wird von Ihrem Browser nicht unterstützt.';
-  }
-}
-
-function showPosition(position) {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-  document.getElementById('locationDisplay').textContent = `Breitengrad: ${latitude}, Längengrad: ${longitude}`;
-}
-
-function showError(error) {
-  switch(error.code) {
-    case error.PERMISSION_DENIED:
-      document.getElementById('locationDisplay').textContent = "Benutzer hat die Anfrage nach Geolocation abgelehnt.";
-      break;
-    case error.POSITION_UNAVAILABLE:
-      document.getElementById('locationDisplay').textContent = "Standortinformationen sind nicht verfügbar.";
-      break;
-    case error.TIMEOUT:
-      document.getElementById('locationDisplay').textContent = "Die Anfrage nach Benutzerstandort ist abgelaufen.";
-      break;
-    case error.UNKNOWN_ERROR:
-      document.getElementById('locationDisplay').textContent = "Ein unbekannter Fehler ist aufgetreten.";
-      break;
-  }
+    watchId = navigator.geolocation.watchPosition(appendLocation);
+  });
+} else {
+  target.innerText = 'Geolocation API not supported.';
 }
 
 
