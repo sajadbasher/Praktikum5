317074d31dcffd61c36c0bee974e2cc9fb04c9c2
Amar-8 <84033938+Amar-8@users.noreply.github.com>
2023-11-09 16:20:15 +0100

    Update app.js
    adding js code to show location

--- js/app.js
+++ js/app.js
@@ -9,6 +9,40 @@ function getUserMedia(options, successCallback, failureCallback) {
   throw new Error('User Media API not supported.');
 }
 
+function trackLocation() {
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError, {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    });
+  } else {
+    document.getElementById('locationDisplay').textContent = 'Geolocation wird von Ihrem Browser nicht unterstützt.';
+  }
+}
+
+function showPosition(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  document.getElementById('locationDisplay').textContent = `Breitengrad: ${latitude}, Längengrad: ${longitude}`;
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      document.getElementById('locationDisplay').textContent = "Benutzer hat die Anfrage nach Geolocation abgelehnt.";
+      break;
+    case error.POSITION_UNAVAILABLE:
+      document.getElementById('locationDisplay').textContent = "Standortinformationen sind nicht verfügbar.";
+      break;
+    case error.TIMEOUT:
+      document.getElementById('locationDisplay').textContent = "Die Anfrage nach Benutzerstandort ist abgelaufen.";
+      break;
+    case error.UNKNOWN_ERROR:
+      document.getElementById('locationDisplay').textContent = "Ein unbekannter Fehler ist aufgetreten.";
+      break;
+  }
+}
 
 
 
