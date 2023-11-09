e22cd238e82ac049ec454f288751174096a8171e
Amar-8 <84033938+Amar-8@users.noreply.github.com>
2023-11-09 16:17:09 +0100

    Update app.js
    updating the file

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-// Define global variables to keep track of the stream and recorder
-var theStream;
-var theRecorder;
-var recordedChunks = [];
 
 // This function initializes user media
 function getUserMedia(options, successCallback, failureCallback) {
@@ -14,115 +10,11 @@ function getUserMedia(options, successCallback, failureCallback) {
 }
 
 
-// This function is called to start the media stream and recording
-function getStream() {
-  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-    alert('User Media API not supported.');
-    return;
-  }
-
-  var constraints = { video: true, audio: true };
-  getUserMedia(constraints, function (stream) {
-    var mediaControl = document.querySelector('video');
-    
-    // Older browsers may not have srcObject
-    if ("srcObject" in mediaControl) {
-      mediaControl.srcObject = stream;
-    } else {
-      // Avoid using this in new browsers, as it is going away.
-      mediaControl.src = window.URL.createObjectURL(stream);
-    }
-    
-    theStream = stream;
-    setupRecorder(stream); // This is a new function to encapsulate the recorder setup
-  }, function (err) {
-    alert('Error: ' + err);
-  });
-}
-function setupRecorder(stream) {
-  try {
-    theRecorder = new MediaRecorder(stream, { mimeType: 'video/webm' });
-    theRecorder.ondataavailable = function(event) {
-      if (event.data.size > 0) {
-        recordedChunks.push(event.data);
-      }
-    };
-    theRecorder.start(100); // Collect 100ms of data chunks
-  } catch (e) {
-    console.error('Exception while creating MediaRecorder:', e);
-    return;
-  }
-  console.log('MediaRecorder created');
-}
-
-// This new function retrieves the video from the cache and downloads it.
-async function downloadFromCache() {
-  const videoKey = 'my_recorded_video.webm';  // This should match the key used when saving the video.
-
-  if (!('caches' in window)) {
-    alert('Cache API not supported!');
-    return;
-  }
-
-  try {
-    const cache = await caches.open('video-cache');
-    const cachedResponse = await cache.match(videoKey);
-    
-    if (!cachedResponse || !cachedResponse.ok) {
-      throw new Error('No cached video found!');
-    }
-
-    const blob = await cachedResponse.blob();
-    const url = window.URL.createObjectURL(blob);
-    
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'downloaded_video.webm';
-    document.body.appendChild(a);
-    a.click();
-    
-    // Cleanup
-    window.URL.revokeObjectURL(url);
-    document.body.removeChild(a);
-  } catch (err) {
-    console.error('Failed to download video from cache:', err);
-    alert(`Error: ${err.message}`);
-  }
-}
 
 
 
-// Stops the recording and saves the video to cache
-function stopRecordingAndSaveToCache() {
-  console.log('Stopping recording and saving data');
-  theRecorder.stop();
-  theStream.getTracks().forEach(track => track.stop());
 
-  theRecorder.onstop = function() {
-    // Create a Blob from the recorded chunks
-    var blob = new Blob(recordedChunks, { type: 'video/webm' });
-    saveToCache(blob);
-  };
-}
 
-// Saves the recording Blob to the cache
-function saveToCache(blob) {
-  if ('caches' in window) {
-    const videoKey = 'my_recorded_video.webm';
-    const request = new Request(videoKey, { mode: 'no-cors' });
-    const response = new Response(blob);
-
-    caches.open('video-cache').then(cache => {
-      cache.put(request, response).then(() => {
-        console.log('Saved video to cache.');
-      }).catch(error => {
-        console.error('Failed to save video to cache:', error);
-      });
-    });
-  } else {
-    console.error('Cache API not supported');
-  }
-}
 
 
 // ... Rest of your code such as service worker registration and notifications ...
