2249e1afdfbdf06bbfb9d58bab63d49e230d5f08
Amar-8 <84033938+Amar-8@users.noreply.github.com>
2023-11-16 16:02:27 +0100

    Update app.js
    storage quotas

--- js/app.js
+++ js/app.js
@@ -19,18 +19,32 @@ function appendLocation(location, verb) {
   target.appendChild(newLocation);
 }
 
-// device motion
+// storage quotas
+if ('storage' in navigator && 'estimate' in navigator.storage) {
+  navigator.storage.estimate()
+    .then(estimate => {
+      document.getElementById('usage').innerHTML = estimate.usage;
+      document.getElementById('quota').innerHTML = estimate.quota;
+      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota).toFixed(0);
+    });
+}
 
+if ('storage' in navigator && 'persisted' in navigator.storage) {
+  navigator.storage.persisted()
+    .then(persisted => {
+      document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+    });
+}
 
-
-
-
-
-function intervalHandler(interval) {
-  document.getElementById("moInterval").innerHTML = interval;
+function requestPersistence() {
+  if ('storage' in navigator && 'persist' in navigator.storage) {
+    navigator.storage.persist()
+      .then(persisted => {
+        document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+      });
+  }
 }
 
 
 
 
-// ... Rest of your code such as service worker registration and notifications ...
