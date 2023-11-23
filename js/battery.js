af30ffbdb0533ef9b92c52001654589eafaa7d70
Amar-8 <84033938+Amar-8@users.noreply.github.com>
2023-11-23 17:26:38 +0100

    Update battery.js
    add real time battery status

--- js/battery.js
+++ js/battery.js
@@ -29,24 +29,31 @@ if ('getBattery' in navigator || ('battery' in navigator && 'Promise' in window)
     batteryPromise = Promise.resolve(navigator.battery);
   }
   
-  batteryPromise.then(function (battery) {
+batteryPromise.then(function (battery) {
+    // Aktualisiert die Anzeige mit den initialen Werten
     document.getElementById('charging').innerHTML = battery.charging ? 'charging' : 'discharging';
     document.getElementById('chargingTime').innerHTML = battery.chargingTime + ' s';
     document.getElementById('dischargingTime').innerHTML = battery.dischargingTime + ' s';
     document.getElementById('level').innerHTML = battery.level;
     
+    // Event-Listener hinzufügen
     battery.addEventListener('chargingchange', onChargingChange);
     battery.addEventListener('chargingtimechange', onChargingTimeChange);
     battery.addEventListener('dischargingtimechange', onDischargingTimeChange);
     battery.addEventListener('levelchange', onLevelChange);
     
+    // Funktion für die Batterie-Level-Änderung
     function onLevelChange() {
-    var level = this.level * 100;
-    handleChange('Battery level changed to ' + level + '%');
-    var batteryCircle = document.getElementById('batteryCircle');
-    batteryCircle.style.setProperty('--level', level + '%');
-    batteryCircle.innerHTML = level + '%';
-}
-  });
+        var level = this.level * 100;
+        handleChange('Battery level changed to ' + level + '%');
+        var batteryCircle = document.getElementById('batteryCircle');
+        batteryCircle.style.setProperty('--level', level + '%');
+        batteryCircle.innerHTML = level + '%';
+    }
+
+    // Initialen Batteriezustand anzeigen
+    onLevelChange.call(battery);
+});
+
 }
 
