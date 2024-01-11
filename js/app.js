c2bff4897b9f381c483c99f5388d645284351922
sajadbasher <104499822+sajadbasher@users.noreply.github.com>
2024-01-11 17:24:18 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,26 +1,43 @@
+function startDrag(e) {
+  this.ontouchmove = this.onmspointermove = moveDrag;
 
-// This function initializes user media
-function getUserMedia(options, successCallback, failureCallback) {
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    return navigator.mediaDevices.getUserMedia(options)
-      .then(successCallback)
-      .catch(failureCallback);
+  this.ontouchend = this.onmspointerup = function () {
+    this.ontouchmove = this.onmspointermove = null;
+    this.ontouchend = this.onmspointerup = null;
   }
-  throw new Error('User Media API not supported.');
+
+  var pos = [this.offsetLeft, this.offsetTop];
+  var that = this;
+  var origin = getCoors(e);
+
+  function moveDrag(e) {
+    var currentPos = getCoors(e);
+    var deltaX = currentPos[0] - origin[0];
+    var deltaY = currentPos[1] - origin[1];
+    this.style.left = (pos[0] + deltaX) + 'px';
+    this.style.top = (pos[1] + deltaY) + 'px';
+    return false; // cancels scrolling
+  }
+
+  function getCoors(e) {
+    var coors = [];
+    if (e.targetTouches && e.targetTouches.length) {
+      var thisTouch = e.targetTouches[0];
+      coors[0] = thisTouch.clientX;
+      coors[1] = thisTouch.clientY;
+    } else {
+      coors[0] = e.clientX;
+      coors[1] = e.clientY;
+    }
+    return coors;
+  }
 }
 
+var elements = document.querySelectorAll('.test-element');
+[].forEach.call(elements, function (element) {
+  element.ontouchstart = element.onmspointerdown = startDrag;
+});
 
-
-
-
-
-
-
-// offline storage
-
-
+document.ongesturechange = function () {
+  return false;
 }
-
-
-
-
